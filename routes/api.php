97f000de44cef61418bcac6b6ddae503959a14f6
MickAntonio <mickantonio2016@gmail.com>
2020-02-28 11:16:43 +0100

    adicionado nova rota a api

--- routes/api.php
+++ routes/api.php
@@ -185,3 +185,5 @@ Route::get('dashboard',         'Api\DashboardController@index');
 // JSON Lista de Municípios
 Route::get('/municipios/{provincia?}', 'Api\MoradasController@municipios');
 Route::get('/municipios-dois/{provincia?}', 'Api\MoradasController@municipios2');
+
+Route::get('/municipios-modelo/{provincia?}', 'Api\MoradasController@municipios2');
